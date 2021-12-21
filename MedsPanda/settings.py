5308ef016e3adc3a6a1f580a4cb754222f48e9c8
Harsh-2811 <hrpatel8935@gmail.com>
2021-12-21 22:35:48 +0530

    css

--- MedsPanda/settings.py
+++ MedsPanda/settings.py
@@ -118,11 +118,11 @@ import os
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static'),
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'static'),
 
-]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL= '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
