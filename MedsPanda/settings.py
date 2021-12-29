b356a3c4ef87adfdc3536bf91a69074b37f070b4
Harsh-2811 <hrpatel8935@gmail.com>
2021-12-29 18:57:42 +0530

    Chnages

--- MedsPanda/settings.py
+++ MedsPanda/settings.py
@@ -135,7 +135,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'medspanda3@gmail.com'
-EMAIL_HOST_PASSWORD = 'hxuljgeudduepmox'
+EMAIL_HOST_PASSWORD = 'exbllybxaistcjrv'
 
 STATIC_URL = 'static/'
 # STATICFILES_DIRS=[
